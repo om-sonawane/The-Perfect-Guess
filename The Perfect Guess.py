3a0ea377fab132ae75e096c0aa47952f92aa1551
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-10-24 05:57:22 -0700

    incerment about the high score

--- The Perfect Guess.py
+++ The Perfect Guess.py
@@ -1,3 +1,17 @@
 import random
-randNumber = random.randint(1,5)
-print(randNumber)
+randNumber = random.randint(1, 100)
+userGuess = None
+guesses = 0
+
+while (userGuess != randNumber):
+    userGuess = int(input("enter your guess: "))
+    guesses += 1
+    if (userGuess == randNumber):
+        print("you guessed it right!")
+    else:
+        if (userGuess > randNumber):
+            print("you guessed it wrong! enter a smaller number")
+        else:
+            print("you guessed it wrong! enter a larger number")
+
+print(f"you guessed the number in  {guesses} guesses")
